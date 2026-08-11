47e30c23efd2dd3d7254dfe51ddfb46850360250
Yashodip Deore <yashodipdeore2006@gmail.com>
2026-08-11 20:42:21 +0530

    chore: setup user routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,21 @@
 import express from 'express';
+import cors from 'cors';
+import cookieParse from 'cookie-parser';
+
+//=== Local modules ===
+import userRoutes from './routes/user.routes';
 
 
 
-
-//============================================
+//======== APP =========
 const app = express();
 
 
 //================ Middleware ==================
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(cookieParse());
 
 
 //=============== Routes =======================
@@ -17,6 +23,13 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.send({ message: 'Welcome to the RideX backend API' });
 });
+
+
+//====== Main Routes =======
+
+app.use('/api/v1/users', userRoutes);
+
+
 
 
 
