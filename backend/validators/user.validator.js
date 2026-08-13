cae0dfbc3a23f639c49caa817935a45da3b1eb65
Yashodip Deore <yashodipdeore2006@gmail.com>
2026-08-13 22:10:01 +0530

    fix: import body from express-validator in user validator

--- backend/validators/user.validator.js
+++ backend/validators/user.validator.js
@@ -1,6 +1,15 @@
-export const registerValidator = [
-  body('email').isEmail().withMessage('Invalid email'),
-  body('fullname.firstname').isLength({ min: 3 }).withMessage('Lastname should be at least 3 characters long'),
-  body('password').isLength({ min: 6 }).withMessage('Password should be at least 6 characters long')
-];
+import { body } from 'express-validator';
 
+export const registerValidator = [
+  body('email')
+    .isEmail()
+    .withMessage('Invalid email'),
+
+  body('fullname.firstname')
+    .isLength({ min: 3 })
+    .withMessage('Firstname should be at least 3 characters long'),
+
+  body('password')
+    .isLength({ min: 6 })
+    .withMessage('Password should be at least 6 characters long')
+];
