728057cccae6708e6ad27df9d66d5263c0de1e44
Yashodip Deore <yashodipdeore2006@gmail.com>
2026-08-15 21:16:22 +0530

    implemented "checkAuthCaptain" for captain

--- backend/src/middleware/checkAuth.js
+++ backend/src/middleware/checkAuth.js
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken';
 
 //=== Local Modules ===
 import userModel from "../models/user.model.js";
+import captainModel from '../models/captain.model.js';
 
 
 export default async function checkAuth(req, res, next) {
@@ -32,5 +33,35 @@ export default async function checkAuth(req, res, next) {
     res.status(500).json({
       error: 'Something went wrong'
     });
+  };
+};
+
+
+export async function checkAuthCaptain(req, res, next) {
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      return res.status(404).json({
+        error: 'Unauthorized Captain/user'
+      });
+    };
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const captain = await captainModel.findById(decoded._id).select('-password');
+
+    if (!captain) {
+      return res.status(404).json({
+        error: 'User not found'
+      });
+    };
+
+    req.captain = captain;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: 'Something went wrong'
+    })
   };
 };
